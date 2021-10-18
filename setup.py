d7fce169bfb99d603c1c335ff65982e7fe861bfa
Zhihao Wang <1397665447@qq.com>
2021-10-18 11:03:25 +0000

    add github

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple pkg to update your yaml configs to args",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/LOOKCC/Yaml2args",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
